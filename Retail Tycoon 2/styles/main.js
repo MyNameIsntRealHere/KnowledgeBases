1a4aa3e39f8d5c324418c67220044d4121948173
MyNameIsntRealHere <remi.timmers@gmail.com>
2025-12-15 16:17:23 +0000

    Improved mobile CSS + Deleted Roblox contact

--- Retail Tycoon 2/styles/main.js
+++ Retail Tycoon 2/styles/main.js
@@ -91,7 +91,7 @@ function setupResponsiveButtons() {
   if (!headerButtons || !sidebar) return;
 
   function moveButtons() {
-    if (window.innerWidth <= 800) {
+    if (window.innerWidth <= 950) {
       if (!sidebar.contains(headerButtons)) {
         sidebar.prepend(headerButtons);
       }
